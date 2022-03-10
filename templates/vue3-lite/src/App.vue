3c08ba6e941dafeaac3983ec7c289012d7864247
oushihao <oushihao97@163.com>
2022-03-10 18:10:58 +0800

    feat(template): vue3 lite template

---
+++ templates/vue3-lite/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from '~/components/HelloWorld.vue'
+</script>
+
+<template>
+  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <HelloWorld />
+    <Footer />
+  </main>
+</template>
---
+++ templates/vue3-lite/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const count = ref(0)
+
+const addHandler = () => count.value++
+</script>
+
+<template>
+  <div>
+    <p text op75>Count: {{ count }}</p>
+    <button m-3 text-sm btn bg-blue @click="addHandler">Add</button>
+  </div>
+</template>
+
+<style scoped></style>
